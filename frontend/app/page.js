2ec0eed229c0aeea12cf525c4d9e5b02642cdcef
irfan7mi <mi2268242@gmail.com>
2025-05-10 18:50:24 +0530

    init commit

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -26,7 +26,7 @@ export default function Home() {
   };
 
   const addDoctor = async () => {
-    const res = await fetch(`hhttps://doctor-app-cyan.vercel.app/api/add-doctor`, {
+    const res = await fetch(`https://doctor-app-cyan.vercel.app/api/add-doctor`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(adds),
